663b29995d8bf1e895365ddd243433d0489eb6a8
ehdbs28 <doyun5373@naver.com>
2023-03-30 10:45:41 +0900

    Revert "Revert "끼릭끼릭끼릭끼릭 && 여러 오류 수정""
    This reverts commit f1a454200b719919ebf22fa2ac021b7fac4db355.

--- Assets/01.Scripts/Doyun/Player/AgentInput.cs
+++ Assets/01.Scripts/Doyun/Player/AgentInput.cs
@@ -19,10 +19,11 @@ public class AgentInput : MonoBehaviour
         {
             Ray ray = CameraManager.Instance.MainCam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            bool isHit = Physics.Raycast(ray, out hit);
+            bool isHit = Physics.Raycast(ray, out hit, CameraManager.Instance.MainCam.farClipPlane, LayerMask.GetMask("Ground"));
 
             if (isHit)
             {
+                Debug.Log(hit.transform.name);
                 OnMouseClickEvent?.Invoke(hit.point);
             }
         }
