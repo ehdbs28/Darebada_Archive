ab240ed71b77701f6f53d79ae98409eae6fb8a6a
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-04-06 01:48:00 +0900

    Camera_rotation

--- Assets/01.Scripts/Doyun/CameraManager/CameraManager.cs
+++ Assets/01.Scripts/Doyun/CameraManager/CameraManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
@@ -30,19 +31,51 @@ public class CameraManager : MonoBehaviour
 
     private void Update()
     {
-        CameraRotate(Input.mousePosition);
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+            CameraRotate(Input.mousePosition);
     }
 
+    // float x = 0, y = 0; // 2번
     private void CameraRotate(Vector3 mousePos)
     {
-        if (mousePos.y > Screen.height - _camBorderThickness)
-            MainCam.transform.rotation = Quaternion.Lerp(MainCam.transform.rotation, Quaternion.Euler(20f, -45f, 0f), Time.deltaTime * _camRotateSpeed);
-        else if (mousePos.y < _camBorderThickness)
-            MainCam.transform.rotation = Quaternion.Lerp(MainCam.transform.rotation, Quaternion.Euler(65f, -45f, 0f), Time.deltaTime * _camRotateSpeed);
 
-        if (mousePos.x > Screen.width - _camBorderThickness)
-            MainCam.transform.rotation = Quaternion.Lerp(MainCam.transform.rotation, Quaternion.Euler(45f, -20f, 0f), Time.deltaTime * _camRotateSpeed);
-        else if (mousePos.x < _camBorderThickness)
-            MainCam.transform.rotation = Quaternion.Lerp(MainCam.transform.rotation, Quaternion.Euler(45f, -65f, 0f), Time.deltaTime * _camRotateSpeed);
+        /* 1. 모서리로 가면 이동하는 방법
+            if (mousePos.y > Screen.height - _camBorderThickness)
+                MainCam.transform.rotation = Quaternion.Lerp(MainCam.transform.rotation, Quaternion.Euler(20f, -45f, 0f), Time.deltaTime * _camRotateSpeed);
+            else if (mousePos.y < _camBorderThickness)
+                MainCam.transform.rotation = Quaternion.Lerp(MainCam.transform.rotation, Quaternion.Euler(65f, -45f, 0f), Time.deltaTime * _camRotateSpeed);
+
+            if (mousePos.x > Screen.width - _camBorderThickness)
+                MainCam.transform.rotation = Quaternion.Lerp(MainCam.transform.rotation, Quaternion.Euler(45f, -20f, 0f), Time.deltaTime * _camRotateSpeed);
+            else if (mousePos.x < _camBorderThickness)
+                MainCam.transform.rotation = Quaternion.Lerp(MainCam.transform.rotation, Quaternion.Euler(45f, -65f, 0f), Time.deltaTime * _camRotateSpeed);
+        */
+
+        /* 2. 마우스를 돌리면 이동하는 방법
+            x += Input.GetAxis("Mouse Y") * _camRotateSpeed;
+            y += Input.GetAxis("Mouse X") * _camRotateSpeed;
+            x = Mathf.Clamp(x, -90, 0);
+            y = Mathf.Clamp(y, -90, 0);
+            transform.localEulerAngles = new Vector3(-x, y, 0);
+        */
+
+        Ray ray = MainCam.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            //MainCam.transform.LookAt(hit.point);
+            Vector3 direction = hit.point - transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 90 * Time.deltaTime * _camRotateSpeed);
+        }
+
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(MainCam.ScreenPointToRay(Input.mousePosition));
     }
 }
