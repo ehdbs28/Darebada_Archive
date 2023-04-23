9f4ec9fb75559f7a98027f998ed44c24035c7a4d
도윤 <doyun5373@naver.com>
2023-04-23 18:15:57 +0900

    0423 낚시 로직 구성중

--- Assets/01.Scripts/Fishing/FishingState.cs
+++ Assets/01.Scripts/Fishing/FishingState.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FishingState : MonoBehaviour
+public abstract class FishingState : MonoBehaviour, IState
 {
-    // Start is called before the first frame update
-    void Start()
+    protected List<FishingTransition> _transitions;
+    protected FishingController _controller;
+
+    public abstract void EnterState();
+    public abstract void ExitState();
+
+
+    public virtual void SetUp(Transform agentRoot)
     {
-        
+        _controller = agentRoot.GetComponent<FishingController>();
+
+        _transitions = new List<FishingTransition>();
+        transform.GetComponentsInChildren<FishingTransition>(_transitions);
+
+        _transitions.ForEach(t => t.SetUp(agentRoot));
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void UpdateState()
     {
-        
+        foreach(var t in _transitions){
+            
+        }
     }
 }
