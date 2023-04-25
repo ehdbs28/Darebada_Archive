c85969fc5d61b22180338eb90d749ca7c98a4a61
ehdbs28 <doyun5373@naver.com>
2023-04-26 00:11:11 +0900

    0426 낚시 로직 거의 만듬 ( 포물선 죽이고 싶다 )

--- Assets/01.Scripts/Fishing/States/FishingThrowingState.cs
+++ Assets/01.Scripts/Fishing/States/FishingThrowingState.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class FishingThrowingState : FishingState
 {
     private Transform _bobberTrm;
+    private Transform _playerTrm;
 
     public override void SetUp(Transform agentRoot)
     {
         base.SetUp(agentRoot);
 
         _bobberTrm = agentRoot.Find("Bobber");
+        _playerTrm = agentRoot.parent;
     }
 
     public override void EnterState()
     {
+        _playerTrm.LookAt(_actionData.LastThrowDirection);
         StartCoroutine(ThrowTo());
     }
 
@@ -24,21 +27,21 @@ public class FishingThrowingState : FishingState
 
     private IEnumerator ThrowTo(){
         Vector3 start = _bobberTrm.position;
-        Vector3 end = _actionData.LastThrowDirection.normalized * (_actionData.LastChargingPower * _actionData.LastChargingPower);
-
-        Debug.Log(start);
-        Debug.Log(end);
+        Vector3 end = _playerTrm.position + _actionData.LastThrowDirection.normalized * _actionData.LastChargingPower;
 
         float throwTime = Mathf.Max(0.3f, Vector3.Distance(start, end)) / _actionData.ThrowingSpeed;
 
         float currentTime = 0f;
         float percent = 0f;
 
+        float v0 = (end - start).y + 9.8f;
+
         while(percent < 1){
-            currentTime += Time.deltaTime;
+            currentTime += _actionData.ThrowingSpeed * Time.deltaTime;
             percent = currentTime / throwTime;
 
-            Vector3 pos = Vector3.Slerp(start, end, percent);
+            Vector3 pos = Vector3.Lerp(start, end, percent);
+            pos.y = start.y + (v0 * percent) +  (-9.8f * percent * percent);
             _bobberTrm.position = pos;
 
             yield return null;
