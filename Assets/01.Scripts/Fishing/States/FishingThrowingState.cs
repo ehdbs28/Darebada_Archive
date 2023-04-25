49f2e1bbd58dd9a45f251b2647d0f3ce2c561ba8
ehdbs28 <doyun5373@naver.com>
2023-04-26 00:27:38 +0900

    0426 포물선 공식 수정

--- Assets/01.Scripts/Fishing/States/FishingThrowingState.cs
+++ Assets/01.Scripts/Fishing/States/FishingThrowingState.cs
@@ -7,6 +7,8 @@ public class FishingThrowingState : FishingState
     private Transform _bobberTrm;
     private Transform _playerTrm;
 
+    private float _gravity = -9.8f;
+
     public override void SetUp(Transform agentRoot)
     {
         base.SetUp(agentRoot);
@@ -27,24 +29,27 @@ public class FishingThrowingState : FishingState
 
     private IEnumerator ThrowTo(){
         Vector3 start = _bobberTrm.position;
-        Vector3 end = _playerTrm.position + _actionData.LastThrowDirection.normalized * _actionData.LastChargingPower;
+        Vector3 end = _bobberTrm.position + (_playerTrm.position + _actionData.LastThrowDirection.normalized * _actionData.LastChargingPower);
+        end.y = 0;
 
         float throwTime = Mathf.Max(0.3f, Vector3.Distance(start, end)) / _actionData.ThrowingSpeed;
 
         float currentTime = 0f;
         float percent = 0f;
 
-        float v0 = (end - start).y + 9.8f;
+        float v0 = (end - start).y - _gravity;
 
         while(percent < 1){
             currentTime += _actionData.ThrowingSpeed * Time.deltaTime;
             percent = currentTime / throwTime;
 
             Vector3 pos = Vector3.Lerp(start, end, percent);
-            pos.y = start.y + (v0 * percent) +  (-9.8f * percent * percent);
+            pos.y = start.y + (v0 * percent) +  (_gravity * percent * percent);
             _bobberTrm.position = pos;
 
             yield return null;
         }
+
+        Debug.Log(_bobberTrm.position);
     }
 }
