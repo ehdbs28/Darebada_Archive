53682b5650dd59269d271d5cf98d81d754692f21
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-04-30 11:41:28 +0900

    MoneyManager_requireStat

--- Assets/01.Scripts/Junho/Money/MoneyManager.cs
+++ Assets/01.Scripts/Junho/Money/MoneyManager.cs
@@ -22,20 +22,44 @@ public class MoneyManager : MonoBehaviour
         if (money < 0) money = 0;
     }
 
-    public void ItemSell()
+    public void ItemSell(object item)
     {
+        // if (item == null) return;
+
+        //money += item.cost;
+
         /*
-         매개변수로 팔 아이템 가져오기
-         매개변수로 가져온 아이템의 가격을 돈에 더하기
+         아이템 가격만큼 돈 추가시키기
          */
     }
 
-    public void ItemUpgrade()
+    public void ItemBuy(object item, object buy)
     {
+        //if (item == null) return;
+        //if (item.cost > money) return;
+        
+        //money -= item.cost;
+        //buy++;
+        
         /*
-         매개변수로 업그레이드 가져오기
-         업그레이드 비용보다 돈이 많으면 업그레이드
-         아니면 취소
+         아이템 가격을 가져오고
+         무엇을 살지 (스텟이라 가정하고 올림)
+         */
+
+    }
+
+    public void ItemUpgrade(object item)
+    {
+        // if (item == null) return;
+        // if (item.cost > money || item.maxUpgrade <= item.upgrade) return;
+
+        // item.upgrade++;    
+        // money -= item.cost;
+
+        /*
+         업그레이드 가격이 돈보다 높거나 업그레이드 수치가 이미 최대면 취소
+         업그레이드 수치 올리기
+         가격만큼 돈에서 빼기
          */
     }
 }
